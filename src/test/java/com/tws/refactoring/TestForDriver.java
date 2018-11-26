ac4e40c23e48a0d7592c463fb2a01f625068044e
bootcamptestkatefan <kate.fan@oocl.com>
2018-11-26 15:19:36 +0800

    Refactor -> Public to Private

---
+++ src/test/java/com/tws/refactoring/TestForDriver.java
@@ -0,0 +1,20 @@
+package com.tws.refactoring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class TestForDriver {
+    @Test
+    void should_check_driver_age() {
+        Driver driver1 = new Driver(19);
+        Driver driver2 = new Driver(17);
+        Police police = new Police();
+
+        assertTrue(police.checkDriver(driver1));
+        assertFalse(police.checkDriver(driver2));
+    }
+
+}
